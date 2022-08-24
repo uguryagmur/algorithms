2963f0966a1a42d6d4e5b74ba7452049c2602b54
uguryagmur <cuy.official@gmail.com>
2022-08-24 16:34:44 +0100

    binary tree in cpp is simplified

--- cpp/test/test_binary_tree.cc
+++ cpp/test/test_binary_tree.cc
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
-#include "binary_tree.h"
+#include "../src/binary_tree.h"
 #include "gtest.h"
 
 TEST(BinaryTreeGenerationTest, GenerationWithPreAndInOrderTraversals)
 {
     std::vector<int> preOrder = {4, 7, 9, 6, 3, 2, 5, 8, 1};
     std::vector<int> inOrder = {7, 6, 9, 3, 4, 5, 8, 2, 1};
-    algo::BinaryTree<int> *tree = algo::BinaryTree<int>::Generator::generateWithPreAndInOrder(preOrder, inOrder);
+    binary_tree::Node<int> *tree = binary_tree::Node<int>::generateWithPreAndInOrder(preOrder, inOrder);
     EXPECT_EQ(preOrder, tree->traversePreOrder());
     EXPECT_EQ(inOrder, tree->traverseInOrder());
-    EXPECT_EQ(3, tree->getTreeHeight());
+    EXPECT_EQ(3, tree->getHeight());
     delete tree;
 }
 
@@ -17,9 +17,9 @@ TEST(BinaryTreeGenerationTest, GenerationWithPostAndInOrderTraversals)
 {
     std::vector<int> postOrder = {6, 3, 9, 7, 8, 5, 1, 2, 4};
     std::vector<int> inOrder = {7, 6, 9, 3, 4, 5, 8, 2, 1};
-    algo::BinaryTree<int> *tree = algo::BinaryTree<int>::Generator::generateWithPostAndInOrder(postOrder, inOrder);
+    binary_tree::Node<int> *tree = binary_tree::Node<int>::generateWithPostAndInOrder(postOrder, inOrder);
     EXPECT_EQ(postOrder, tree->traversePostOrder());
     EXPECT_EQ(inOrder, tree->traverseInOrder());
-    EXPECT_EQ(3, tree->getTreeHeight());
+    EXPECT_EQ(3, tree->getHeight());
     delete tree;
 }
