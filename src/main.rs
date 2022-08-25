19b4a7db801633a08483396658a56184cb31232e
uguryagmur <cuy.official@gmail.com>
2022-08-26 00:16:40 +0100

    main branch is for algorithm implementations in rust

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 use crate::graph::GraphNode;
+use crate::graph::Graph;
 
 pub mod binary_search_tree;
 pub mod binary_tree;
 pub mod graph;
-mod tests {
-    mod test_binary_search_tree;
-    mod test_binary_tree;
-}
 
 fn main() {
     let g0 = GraphNode::create_with_id(0);
@@ -15,6 +12,6 @@ fn main() {
     let g3 = GraphNode::create_with_id(3);
     let g4 = GraphNode::create_with_id(4);
     let edges = [(&g0, &g1), (&g0, &g2), (&g1, &g3), (&g2, &g4), (&g3, &g4)];
-    let graph = graph::UndirectedGraph::create(&edges);
-    graph.dfs(&g0);
+    let graph = graph::UndirectedGraph::new(&edges);
+    println!("{:?}", graph::dfs(&graph, &g0))
 }
